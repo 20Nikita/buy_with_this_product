b3d3c8c0640414adef1384d70438aebcc0484a06
Violonur-PavelBI <pacha.znaet.vse@yandex.ru>
2023-12-07 17:47:15 +0300

    black

--- interfase.py
+++ interfase.py
@@ -12,8 +12,8 @@ height_Goods = 50
 width_Goods = 150
 
 
-f = open('names.txt', 'r', encoding='utf-8')
-data=[]
+f = open("names.txt", "r", encoding="utf-8")
+data = []
 for i in f.readlines():
     data.append(i[:-1])
 
@@ -23,55 +23,87 @@ N_column = 10
 N_row = 12
 Radio = 0
 N = 1370
-num_level=1
-hide_neuron=32
+num_level = 1
+hide_neuron = 32
 per_net = perceptron_net.PerceptronNet(N).to(device)
-lstm_net = lstm.LSTM(N+1,hide_neuron,num_level)
+per_net = perceptron_net.load_weights(per_net, "model_pers.tar")
+lstm_net = lstm.LSTM(N + 1, hide_neuron, num_level)
+lstm_net = perceptron_net.load_weights(per_net, "model_lstm.tar")
 with dpg.font_registry():
-    with dpg.font(f'C:\\Windows\\Fonts\\trebucbd.ttf', 13, default_font=True, tag="Default font") as f:
+    with dpg.font(
+        f"C:\\Windows\\Fonts\\trebucbd.ttf", 13, default_font=True, tag="Default font"
+    ) as f:
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
-    
+
 dpg.bind_font("Default font")
 
 Basket = []
-tag_product = 'avQEV'
+tag_product = "avQEV"
+
+
 def add_data():
     global ind
     for i in range(N_row):
         with dpg.table_row(parent="Goods"):
             for i in range(N_column):
-                dpg.add_button(label=data[ind], tag=str(ind),height=height_Goods,width=width_Goods, callback=add_basket, user_data=ind); ind+=1
-    
+                dpg.add_button(
+                    label=data[ind],
+                    tag=str(ind),
+                    height=height_Goods,
+                    width=width_Goods,
+                    callback=add_basket,
+                    user_data=ind,
+                )
+                ind += 1
+
 
 def add_basket(sender, add_data, user_data):
     Basket.append(user_data)
     tag = str(random.random())
-    with dpg.group(horizontal=True, parent='Basket', tag=tag):
-        dpg.add_button(label=data[user_data],height=height_Goods,width=width_Goods, callback=remowe_basket, user_data=[tag,user_data])
+    with dpg.group(horizontal=True, parent="Basket", tag=tag):
+        dpg.add_button(
+            label=data[user_data],
+            height=height_Goods,
+            width=width_Goods,
+            callback=remowe_basket,
+            user_data=[tag, user_data],
+        )
     get_stat_product()
     # print(Basket)
+
 
 def remowe_basket(sender, add_data, user_data):
     dpg.delete_item(user_data[0])
     Basket.remove(user_data[1])
     get_stat_product()
 
+
 def get_stat_product():
     global tag_product
-    if len(Basket)>0:
+    if len(Basket) > 0:
         if Radio == 0:
             n_product = statistic_predict.statistic_predict(Basket, N, N_column)
         elif Radio == 1:
             n_product = perceptron_net.predict(Basket, per_net, N, N_column)
         elif Radio == 2:
-            n_product = lstm.predict(Basket, lstm_net, N, N_column,num_level,hide_neuron)
+            n_product = lstm.predict(
+                Basket, lstm_net, N, N_column, num_level, hide_neuron
+            )
         dpg.delete_item(tag_product)
         tag_product = str(random.random())
-        with dpg.group(horizontal=True, parent='product', tag=tag_product):
+        with dpg.group(horizontal=True, parent="product", tag=tag_product):
             for i in n_product:
-                dpg.add_button(label=data[i],height=height_Goods,width=width_Goods, callback=add_basket, user_data=i)
+                dpg.add_button(
+                    label=data[i],
+                    height=height_Goods,
+                    width=width_Goods,
+                    callback=add_basket,
+                    user_data=i,
+                )
     else:
         dpg.delete_item(tag_product)
+
+
 def radio(sender, add_data, user_data):
     global Radio
     if add_data == "Cтатистика":
@@ -82,42 +114,74 @@ def radio(sender, add_data, user_data):
         Radio = 2
     get_stat_product()
 
+
 with dpg.window(tag="Window"):
-    dpg.add_text('Корзина')
-    with dpg.child_window(width=N_column*(width_Goods+10), height=height_Goods+16, horizontal_scrollbar =True):
-        with dpg.group(horizontal=True, tag='Basket'):
+    dpg.add_text("Корзина")
+    with dpg.child_window(
+        width=N_column * (width_Goods + 10),
+        height=height_Goods + 16,
+        horizontal_scrollbar=True,
+    ):
+        with dpg.group(horizontal=True, tag="Basket"):
             pass
 
     dpg.add_button(label="Купить")
 
-    dpg.add_text('Товары')
-    with dpg.table(tag='Goods', header_row=False, borders_innerH=True, 
-                    borders_outerH=True, borders_innerV=True, borders_outerV=True,
-                    height=N_row*(height_Goods+5),width=N_column*(width_Goods+10), scrollY=True):
-        
+    dpg.add_text("Товары")
+    with dpg.table(
+        tag="Goods",
+        header_row=False,
+        borders_innerH=True,
+        borders_outerH=True,
+        borders_innerV=True,
+        borders_outerV=True,
+        height=N_row * (height_Goods + 5),
+        width=N_column * (width_Goods + 10),
+        scrollY=True,
+    ):
+
         for i in range(N_column):
             dpg.add_table_column()
         for i in range(N_row):
             with dpg.table_row():
                 for i in range(N_column):
-                    dpg.add_button(label=data[ind], tag=str(ind),height=height_Goods,width=width_Goods, callback=add_basket, user_data=ind); ind+=1
-        
-        
+                    dpg.add_button(
+                        label=data[ind],
+                        tag=str(ind),
+                        height=height_Goods,
+                        width=width_Goods,
+                        callback=add_basket,
+                        user_data=ind,
+                    )
+                    ind += 1
+
     dpg.add_button(label="Еще", callback=add_data)
- 
-    dpg.add_text('С этим товаром покупают:')
+
+    dpg.add_text("С этим товаром покупают:")
     with dpg.group(horizontal=True):
-        dpg.add_text('Метод оценки:')
-        dpg.add_radio_button(("Cтатистика", "Перцерптрон", "LSTM"), callback=radio, horizontal=True, tag="radio")
-    with dpg.child_window(autosize_x=False, height=height_Goods+16,width=N_column*(width_Goods+10), horizontal_scrollbar =True):
-        with dpg.group(horizontal=True, tag='product'):
+        dpg.add_text("Метод оценки:")
+        dpg.add_radio_button(
+            ("Cтатистика", "Перцерптрон", "LSTM"),
+            callback=radio,
+            horizontal=True,
+            tag="radio",
+        )
+    with dpg.child_window(
+        autosize_x=False,
+        height=height_Goods + 16,
+        width=N_column * (width_Goods + 10),
+        horizontal_scrollbar=True,
+    ):
+        with dpg.group(horizontal=True, tag="product"):
             pass
 
 
 # dpg.show_item_registry()
-dpg.create_viewport(title='Buy with this product', width=N_column*(width_Goods+10)+50, height=1000)
+dpg.create_viewport(
+    title="Buy with this product", width=N_column * (width_Goods + 10) + 50, height=1000
+)
 dpg.setup_dearpygui()
-dpg.set_primary_window('Window', True)
+dpg.set_primary_window("Window", True)
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
